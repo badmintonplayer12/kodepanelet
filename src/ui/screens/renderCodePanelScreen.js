62f63d4352736a6b2ae5e965d2aeb3010a5c88d3
badmintonplayer12 <lea.garpestad@gmail.com>
2026-05-22 18:24:59 +0200

    Add code panel screen renderer

---
+++ src/ui/screens/renderCodePanelScreen.js
@@ -0,0 +1,18 @@
+import { createElement } from '../dom.js';
+import { createKeypad } from '../components/createKeypad.js';
+import { createScreenFrame } from '../components/createScreenFrame.js';
+
+export function renderCodePanelScreen({ enteredCode, onKeyPress }) {
+  const frame = createScreenFrame('Agentkode', 'Tast inn koden for å åpne låsen.');
+  const display = createElement('output', {
+    className: 'code-display',
+    text: maskCode(enteredCode),
+  });
+
+  frame.append(display, createKeypad(onKeyPress));
+  return frame;
+}
+
+function maskCode(code) {
+  return code ? '●'.repeat(code.length) : 'KODE';
+}
