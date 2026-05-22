13f56e5a560df75d61b97bc217b46a6b742c4130
badmintonplayer12 <lea.garpestad@gmail.com>
2026-05-22 19:15:39 +0200

    Allow screen frame without subtitle

--- src/ui/components/createScreenFrame.js
+++ src/ui/components/createScreenFrame.js
@@ -1,10 +1,14 @@
 import { createElement } from '../dom.js';
 
-export function createScreenFrame(title, subtitle) {
+export function createScreenFrame(title, subtitle = '') {
   const frame = createElement('section', { className: 'screen-frame' });
   const heading = createElement('h1', { text: title });
-  const lead = createElement('p', { className: 'screen-frame__lead', text: subtitle });
 
-  frame.append(heading, lead);
+  frame.append(heading);
+
+  if (subtitle) {
+    frame.append(createElement('p', { className: 'screen-frame__lead', text: subtitle }));
+  }
+
   return frame;
 }
