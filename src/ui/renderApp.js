dd6331f6172c5d4268172da52cbbb901633b4b56
badmintonplayer12 <lea.garpestad@gmail.com>
2026-05-22 18:25:24 +0200

    Add app renderer

---
+++ src/ui/renderApp.js
@@ -0,0 +1,24 @@
+import { SCREENS } from '../app/screens.js';
+import { RESULT_TYPES } from '../core/resultTypes.js';
+import { clearElement } from './dom.js';
+import { renderCodePanelScreen } from './screens/renderCodePanelScreen.js';
+import { renderResultScreen } from './screens/renderResultScreen.js';
+import { renderSetupScreen } from './screens/renderSetupScreen.js';
+
+export function renderApp(appRoot, state, actions) {
+  clearElement(appRoot);
+  appRoot.append(createScreen(appRoot, state, actions));
+}
+
+function createScreen(appRoot, state, actions) {
+  if (state.screen === SCREENS.setup) {
+    return renderSetupScreen({ appRoot, onCodeSet: actions.setCode });
+  }
+
+  if (state.screen === SCREENS.codePanel) {
+    return renderCodePanelScreen({ enteredCode: state.enteredCode, onKeyPress: actions.pressKey });
+  }
+
+  const resultType = state.screen === SCREENS.success ? RESULT_TYPES.success : RESULT_TYPES.error;
+  return renderResultScreen({ resultType, onReset: actions.resetAttempt });
+}
