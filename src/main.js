5b284c44f13914de5c1343158d8cb96a81772d81
badmintonplayer12 <lea.garpestad@gmail.com>
2026-05-24 00:45:20 +0200

    Register service worker from main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from './app/createApp.js';
+import { registerServiceWorker } from './pwa/registerServiceWorker.js';
 
 const appRoot = document.querySelector('#app');
 
 createApp(appRoot).start();
+registerServiceWorker();
