8c08ae80f4d80aa2ed396ef8e2c0033a8bf5037a
badmintonplayer12 <lea.garpestad@gmail.com>
2026-05-24 00:03:43 +0200

    Show countdown on code panel

--- src/ui/screens/renderCodePanelScreen.js
+++ src/ui/screens/renderCodePanelScreen.js
@@ -1,15 +1,16 @@
 import { createElement } from '../dom.js';
+import { createCountdownDisplay } from '../components/createCountdownDisplay.js';
 import { createKeypad } from '../components/createKeypad.js';
 import { createScreenFrame } from '../components/createScreenFrame.js';
 
-export function renderCodePanelScreen({ enteredCode, onKeyPress }) {
+export function renderCodePanelScreen({ enteredCode, remainingSeconds, onKeyPress }) {
   const frame = createScreenFrame('Sikkerhetslås');
   const display = createElement('output', {
     className: 'code-display',
     text: formatCode(enteredCode),
   });
 
-  frame.append(display, createKeypad(onKeyPress));
+  frame.append(createCountdownDisplay(remainingSeconds), display, createKeypad(onKeyPress));
   return frame;
 }
 
